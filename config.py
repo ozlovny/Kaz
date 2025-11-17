0c22187eeaf5ebd8f9449723553a1eb003f6af41
ozlovny <olegsmirnov12220@gmail.com>
2025-11-17 20:19:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
-TOKEN = "7649006146:AAFE5gAQa-a9OSy9JOLavO9MDzcbkV240xM" #токен BotFather
+TOKEN = "8500850729:AAEQUTSRXIVSM2omL_8zqIHK7Rv1w4M0oqY" #токен BotFather
 CRYPTO_TOKEN = "358333:AAivRR5Rc0zM0RQDBZ1mcUbUydoinvEF0oz" #токен CreptoPay
 LOG_CHANNEL = -1003312958841 #ЛОГИРОВАНИЕ
-CHANNEL_BROKER = -1003140470097 #Посредник
+CHANNEL_BROKER = -1002406027808 #Посредник
 MAIN_CHANNEL = -1003312784754 #основа
 ADMINS = [6677500867] #список админов
-CHECK_URL = "" #юрл счёта
+CHECK_URL = "http://t.me/send?start=IVD7Y5BpR9M2" #юрл счёта
 MONEYBACK = 0 #процент манибэка от игры
+
 
